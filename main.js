3fa048f3bff568267efaa98719e1e114ebf88b1c
Tom <tsanchis@edenschool.fr>
2022-05-31 17:17:28 +0200

    project

---
+++ main.js
@@ -0,0 +1,32 @@
+// Menu Burger
+
+const burger = document.querySelector(".burger");
+const Menu = document.querySelector(".menu");
+
+burger.addEventListener("click", () => {
+  Menu.classList.toggle("hide");
+});
+
+// Swipper
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
